e235d6036c0dcb0277e64185c077f82919bef5f2
rawani123 <rawoolaniruddha3@gmail.com>
2022-12-22 10:52:23 +0530

    ani

---
+++ AB.java
@@ -0,0 +1,20 @@
+class AB{
+public static void main(String a[]){
+try{
+int a[];
+a= new int[10];
+a[12]=20;
+int d=0;
+int c= 42/d;
+System.out.println(a);
+}
+catch(ArrayIndexOutofBoundsException e){
+System.out.println(e);
+}
+catch(ArithmeticException e){
+System.out.println(e);
+}
+catch(Exception e){
+System.out.println(e);
+}
+}}
